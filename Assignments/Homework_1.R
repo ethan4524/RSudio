5cbdc89c968a607cc2dc6f0250c32cdeee1bdf3a
Ethan <ecb78379@uga.edu>
2024-08-26 15:32:08 -0400

    completed part 1 of homework

--- Assignments/Homework_1.R
+++ Assignments/Homework_1.R
@@ -1,3 +1,5 @@
+rm(list = ls())
+
 library(tidyverse)
 library(ggpubr)
 setwd("C:/Users/Ethan/OneDrive/Documents/BIOE8510/Assignments")
@@ -28,3 +30,58 @@ c=pi^2
 d=log(20.1)
 e=3.3^4
 A=list(a,b,c,d,e)
+
+####Problem 3
+# A
+v=seq(0,100,by=10)
+time = c(v,v,v,v)
+# B
+expnum=c()
+for (i in 1:4) {
+  for (j in 0:10) {
+    expnum=c(expnum,i)
+  }
+}
+# C
+baseline=runif(length(time))
+# D
+mydf=data.frame(time,expnum,baseline)
+# E
+print(colnames(mydf))
+# F
+dim(mydf)
+# G
+response = rnorm(n = length(mydf$time), mean = 5, sd = 0.5)
+mydf['response'] = response
+# H
+mydf['difference'] = mydf$response - mydf$baseline
+# I
+exp2 = subset(mydf, expnum==2)
+# J
+time0 = subset(mydf, time==0)
+# K
+exp4=subset(mydf,expnum==4)
+temp=data.frame(subset(exp4,time==50))
+print(temp$difference)
+# L
+responders=subset(subset(mydf,response>5), time==100)
+# M
+testsample=subset(mydf, (response < 5) & (baseline > 0.5))
+# N
+mean_difference=mean(mydf$difference)
+# O
+sum_baseline=sum(mydf$baseline)
+# P
+new_row = data.frame(
+  time=0,
+  expnum=5,
+  baseline=0,
+  response=5,
+  difference=5
+)
+mydf = rbind(mydf, new_row)
+# Q
+exp1_70=(subset(mydf, (expnum==1 & time==70)))$response
+exp3_70=(subset(mydf, (expnum==3 & time==70)))$response
+diff=(exp1_70-exp3_70)
+
--- README.md
+++
@@ -1,4 +0,0 @@
-# Systems Modeling of Physiology and Pharmacology
-### RStudio Files
-
-This is a repo for the Systems Modeling of Physiology and Pharmacology class.
